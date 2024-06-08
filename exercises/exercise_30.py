74c9686861995045ea034e2c43250cb83be3fc8c
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-08 12:07:18 -0400

    Update exercise_30.py

--- exercises/exercise_30.py
+++ exercises/exercise_30.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 n = int(input())
 emojis = list(input())
 
@@ -7,15 +9,24 @@ unicode_str_start = int('1F600', 16)
 unicode_str_finish = int('1F64F', 16)
 
 emojis_list = [chr(unicode_str) for unicode_str in range(unicode_str_start, unicode_str_finish)]
+emojis_dict = {idx: emoji for idx, emoji in enumerate(emojis_list)}
+
+pprint(emojis_dict)
 
 new_emojis = ""
 
+# print(f"{len(emojis_list) = }")
 
-while n > len(emojis_list):
-    n -= len(emojis_list)
+# while n // len(emojis_list) >= 1:
+#     n -= len(emojis_list)
 
 
 for emoji in emojis:
-    new_emojis += emojis_list[emojis_list.index(emoji) + n]
+    # print("Current emoji")
+    # print(f"{emoji} - {emojis_list.index(emoji)}")
+    new_emojis += emojis_list[emojis_list.index(emoji) + n % len(emojis_list) + 1]
+    # print(f"{emojis_list[emojis_list.index(emoji) + n % len(emojis_list)]} - {emojis_list.index(emoji) + n % len(emojis_list) + 1}")
+    # print()
 
 print(new_emojis)
+
