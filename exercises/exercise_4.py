7b5f9ef4e52365c75f38007fca96fdbd1bb98809
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-07 21:25:32 -0400

    Update exercise_4.py

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,9 @@
-# Your solution to Exercise 4
+lst = list(map(int, input().split(",")))
 
+output = []
+
+for num in lst:
+    if num % 2 == 1:
+        output.append(str(num))
+
+print(' '.join(output))
