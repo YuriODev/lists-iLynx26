908699e9232f9439ab1fa109a1498fdbdbe40ae6
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-07 21:25:00 -0400

    Update exercise_18.py

--- exercises/exercise_18.py
+++ exercises/exercise_18.py
@@ -1,2 +1,12 @@
-# Your solution to Exercise 18
+lst = list(map(int, input().split()))
 
+min_value = min(lst)
+min_index = lst.index(min_value)
+
+max_value = max(lst)
+max_index = lst.index(max_value)
+
+lst[min_index] = max_value
+lst[max_index] = min_value
+
+print(" ".join(map(str, lst)))
