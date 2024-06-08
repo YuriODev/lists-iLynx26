37360a3396fb2efdcb471ae98259422a0da55ff0
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-07 21:25:19 -0400

    Update exercise_26.py

--- exercises/exercise_26.py
+++ exercises/exercise_26.py
@@ -1,2 +1,12 @@
-# Your solution to Exercise 26
+lst = list(map(int, input().split()))
 
+even_nums = []
+odd_nums = []
+
+for num in lst:
+    if num % 2 == 0:
+        even_nums.append(num)
+    else:
+        odd_nums.append(num)
+
+print(" ".join(list(map(str, sorted(even_nums, reverse=True)+sorted(odd_nums)))))
