07bc0d450e78a4df9c0385faf577575e35de75c2
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-08 12:34:09 -0400

    Update exercise_33.py

--- exercises/exercise_33.py
+++ exercises/exercise_33.py
@@ -1,2 +1,15 @@
-# Your solution to Exercise 33
+number = input()
+number_lst = list(number)
 
+max_number = 0
+
+for i in range(len(number_lst)):
+    new_lst = []
+    for j in range(len(number_lst)):
+        if i!=j:
+            new_lst += number_lst[j]
+    new_number = int(''.join(new_lst))
+    if new_number > max_number:
+        max_number = new_number
+
+print(max_number)
