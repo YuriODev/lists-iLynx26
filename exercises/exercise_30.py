093b6932348c0c8200c3bd5e2423e1ddbb754d2e
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-08 12:24:49 -0400

    Update exercise_30.py

--- exercises/exercise_30.py
+++ exercises/exercise_30.py
@@ -6,7 +6,7 @@ emojis = list(input())
 new_emojis = ""
 
 unicode_str_start = int('1F600', 16)
-unicode_str_finish = int('1F64F', 16)
+unicode_str_finish = int('1F64F', 16) + 1
 
 emojis_list = [chr(unicode_str) for unicode_str in range(unicode_str_start, unicode_str_finish)]
 emojis_dict = {idx: emoji for idx, emoji in enumerate(emojis_list)}
@@ -24,9 +24,7 @@ new_emojis = ""
 for emoji in emojis:
     # print("Current emoji")
     # print(f"{emoji} - {emojis_list.index(emoji)}")
-    idx = emojis_list.index(emoji) + n % len(emojis_list)
-    if n < 0:
-        idx += 1
+    idx = (emojis_list.index(emoji) + n) % len(emojis_list)
     new_emojis += emojis_list[idx]
     # print(f"{emojis_list[emojis_list.index(emoji) + n % len(emojis_list)]} - {emojis_list.index(emoji) + n % len(emojis_list) + 1}")
     # print()
