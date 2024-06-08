6b4be06657e291c3c49e77456856edab0fad0e5c
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-08 11:35:43 -0400

    Update exercise_4.py

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -3,7 +3,7 @@ lst = list(map(int, input().split(",")))
 output = []
 
 for i in range(len(lst)):
-    if i % 2 == 0:
+    if i % 2 == 1:
         output.append(str(lst[i]))
 
 print(','.join(output))
