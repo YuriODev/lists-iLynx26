63658f96516ec790a3050a78010b1b5bc3ac988e
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-07 21:25:40 -0400

    Update exercise_6.py

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,13 @@
-# Your solution to Exercise 6
+lst = list(map(int, input().split()))
 
+greater_then_neighbours = 0
+
+for i in range(1, len(lst)-1):
+    num = lst[i]
+    if lst[i] != -1:
+        if num > lst[i-1] and num > lst[i+1]:
+            greater_then_neighbours += 1
+    else:
+        break
+
+print(greater_then_neighbours)
