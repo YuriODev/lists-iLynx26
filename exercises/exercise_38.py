4aad91ded467e62f182f3fd9dd46b416d21ec58e
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-08 13:00:38 -0400

    Update exercise_38.py

--- exercises/exercise_38.py
+++ exercises/exercise_38.py
@@ -1,2 +1,12 @@
-# Your solution to Exercise 38
+number = list(input())
 
+max_number = sorted(number, reverse = True)
+
+min_number = sorted(number)
+
+if "0" in min_number:
+    count_zeros = min_number.count("0")
+    first_element = min_number[count_zeros]
+    min_number = first_element + "0" * count_zeros + "".join(min_number[count_zeros+1:])
+
+print("".join(min_number) + " " + "".join(max_number))
