780f5b9441b679a09494aa623cdf81a434b09567
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-22 11:23:50 -0400

    Update exercise_32.py

--- exercises/exercise_32.py
+++ exercises/exercise_32.py
@@ -4,3 +4,18 @@ lst = []
 
 for i in range(num):
     lst.append(input())
+
+max_length = 0
+
+for element in lst:
+     if len(element) > max_length:
+          max_length = len(element)
+
+list_of_indices = []
+
+for i in range(len(lst)):
+    element = lst[i]
+    if len(element) == max_length:
+          list_of_indices.append(str(i+1))
+
+print("\n".join(list_of_indices))
