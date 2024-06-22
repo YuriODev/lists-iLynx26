23531f8e0bf92f1c25b7f6f9e76738444a7d15ce
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-22 11:31:14 -0400

    Update exercise_31.py

--- exercises/exercise_31.py
+++ exercises/exercise_31.py
@@ -1,2 +1,16 @@
-# Your solution to Exercise 31
+lst = list(map(int, input().split()))
 
+if len(lst) == 1:
+    print(lst[0])
+else:
+    new_lst = [0] * len(lst)
+
+    for i in range(len(lst)):
+        if i == 0:
+            new_lst[0] = lst[1] + lst[-1]
+        elif i == len(lst) - 1:
+            new_lst[-1] = lst[-2] + lst[0]
+        else:
+            new_lst[i] = lst[i-1] + lst[i+1]
+
+    print(" ".join(list(map(str, new_lst))))
