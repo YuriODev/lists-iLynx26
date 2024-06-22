ee06facf77382683b711ee9d833e0acfcb0bdf3b
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-22 11:12:42 -0400

    Update exercise_37.py

--- exercises/exercise_37.py
+++ exercises/exercise_37.py
@@ -1,2 +1,24 @@
-# Your solution to Exercise 37
+string = input()
 
+new_list = []
+
+for i in range(len(string)):
+    symbol = string[i]
+    if symbol.isdigit():
+        if string[i+1].isdigit():
+            if string[i+2].isdigit():
+                lst = string[i+3] * (int(symbol)*100 + int(string[i+2]) * 10 + int(string[i+1])-1)
+                new_list.append(lst)
+                i += 3
+            else:
+                lst = string[i+2]*(int(symbol)*10 + int(string[i+1])-1)
+                new_list.append(lst)
+                i += 2
+        else:
+            lst = string[i+1]*(int(symbol)-1)
+            new_list.append(lst)
+            i += 1
+    else:
+        new_list.append(symbol)
+
+print("".join(new_list))
