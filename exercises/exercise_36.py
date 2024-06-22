5d36d821d4a547614bfcee0daf86eae3efc5d194
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-22 11:12:39 -0400

    Update exercise_36.py

--- exercises/exercise_36.py
+++ exercises/exercise_36.py
@@ -1,2 +1,13 @@
-# Your solution to Exercise 36
+lst = input().split()
+num = input()
 
+if num not in lst:
+    print(None)
+else:
+    indexes = []
+    for i in range(lst.count(num)):
+        index = lst.index(num)
+        indexes.append(str(index+1))
+        lst[index] = "*"
+
+    print(" ".join(indexes))
