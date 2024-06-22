5102aec033eb7d5c8e1916603ef52a1f1f23d31d
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-06-22 11:12:33 -0400

    Update exercise_34.py

--- exercises/exercise_34.py
+++ exercises/exercise_34.py
@@ -1,2 +1,15 @@
-# Your solution to Exercise 34
+lst = input().split()
 
+max_length = 0
+
+for word in lst:
+    if len(word) > max_length:
+        max_length = len(word)
+
+num_lst = []
+
+for word in lst:
+    if len(word) == max_length:
+        num_lst.append(word)
+
+print(" ".join(num_lst))
